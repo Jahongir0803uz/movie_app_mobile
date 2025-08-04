d484b38cfb995ad26d03cda22017f60b8b0fa2de
Jahongir0803uz <samadovjahongir0475@gmail.com>
2025-08-04 15:23:46 +0500

    profil qismini bajar

--- lib/home_screen/home_screen.dart
+++ lib/home_screen/home_screen.dart
@@ -1,38 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
+import 'profile_page/profile_screen.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: Image.asset('assets/images/screen_page_image.png'),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: SvgPicture.asset(
-              'assets/icons/search.svg',
-              height: 20,
-              width: 20,
+    return SafeArea(
+      top: false,
+      bottom: false,
+      child: Scaffold(
+        appBar: AppBar(
+          leading: Image.asset('assets/images/screen_page_image.png'),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: SvgPicture.asset(
+                'assets/icons/search.svg',
+                height: 20,
+                width: 20,
+              ),
             ),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: SvgPicture.asset(
-              'assets/icons/notification.svg',
-              height: 40,
-              width: 40,
+            IconButton(
+              onPressed: () {},
+              icon: SvgPicture.asset(
+                'assets/icons/notification.svg',
+                height: 40,
+                width: 40,
+              ),
             ),
-          ),
-        ],
-        backgroundColor: Colors.transparent,
-      ),
-      extendBodyBehindAppBar: true,
-      body: SafeArea(
-        top: false,
-        child: Column(
+          ],
+          backgroundColor: Colors.transparent,
+        ),
+        extendBodyBehindAppBar: true,
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_filled),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: IconButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ProfileScreen(),
+                      ),
+                    );
+                  },
+                  icon: Icon(
+                      Icons.person_sharp,
+                  ),
+              ),
+              label: 'Profile',
+            ),
+          ],
+        ),
+        body: Column(
           children: [
             Expanded(
               child: Container(
