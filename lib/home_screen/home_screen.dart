291c7c2d9ae292a5ddf3f10d2555a5c1bb115bf0
shaxa51526 <shaxruzuktamov101208@gmail.com>
2025-08-04 13:58:58 +0500

    image bug removed

--- lib/home_screen/home_screen.dart
+++ lib/home_screen/home_screen.dart
@@ -8,13 +8,27 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         top: false,
-          child: Column(
-            children: [
-              DecoratedBox(decoration: BoxDecoration(
-                image: DecorationImage(image: NetworkImage('https://static0.gamerantimages.com/wordpress/wp-content/uploads/2021/11/demon-slayer-tanjiro-mugen-train.jpg?q=70&fit=contain&w=1200&h=628&dpr=1'))
-              ))
-            ],
-          ))
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      'https://static0.gamerantimages.com/wordpress/wp-content/uploads/2021/11/demon-slayer-tanjiro-mugen-train.jpg?q=50&fit=crop&w=1100&h=618&dpr=1.5',
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
